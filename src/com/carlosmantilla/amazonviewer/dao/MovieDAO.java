a34e8ebfb733d8149845f389fa3f2be3858dc1c3
carlosmantilla23 <carlos.mantilla.jaimes@gmail.com>
2019-02-20 23:08:40 -0500

    Sentencia SELECT

--- src/com/carlosmantilla/amazonviewer/dao/MovieDAO.java
+++ src/com/carlosmantilla/amazonviewer/dao/MovieDAO.java
@@ -19,16 +19,18 @@ public interface MovieDAO extends IDBConnection {
 		ArrayList<Movie> movies = new ArrayList<Movie>();
 
 		try (Connection connection = connectToDB()) {
-			String query = "SELECT * FROM " + TMOVIE;;
+			String query = "SELECT * FROM " + TMOVIE;
+			;
 			PreparedStatement preparedStatement = connection.prepareStatement(query);
 			ResultSet rs = preparedStatement.executeQuery();
-				
-			
+
 			while (rs.next()) {
 				Movie movie = new Movie(rs.getString(TMOVIE_TITLE), rs.getString(TMOVIE_GENRE),
 						rs.getString(TMOVIE_CREATOR), Integer.valueOf(rs.getString(TMOVIE_DURATION)),
 						Short.valueOf(rs.getString(TMOVIE_YEAR)));
 				movie.setId(Integer.valueOf(rs.getString(TMOVIE_ID)));
+				movie.setViewed(
+						getMovieViewed(preparedStatement, connection, Integer.valueOf(rs.getString(TMOVIE_ID))));
 				movies.add(movie);
 
 			}
@@ -38,8 +40,25 @@ public interface MovieDAO extends IDBConnection {
 		return movies;
 	}
 
-	default boolean getMovieViewed() {
-		return false;
+	default boolean getMovieViewed(PreparedStatement preparedStatement, Connection connection, int id_movie) {
+		boolean viewedFlag = false;
+		String query = "SELECT * FROM " + TVIEWED + " WHERE " + TVIEWED_IDMATERIAL + "= ? " + " AND " + TVIEWED_IDELEMENT
+				+ "= ?" + " AND " + TVIEWED_IDUSUARIO + "= ?";
+		ResultSet rs = null;
+		try {
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, ID_TMATERIALS[0]);
+			preparedStatement.setInt(2, id_movie);
+			preparedStatement.setInt(3, TUSER_IDUSUARIO);
+
+			rs = preparedStatement.executeQuery();
+			viewedFlag = rs.next();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return viewedFlag;
 	}
 
 }
